829e4521f78c82574e204e4076f44312eac23c7f
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2021-06-07 06:27:58 -0400

    Working?

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function loadAPI() {
         token: '5b1064585f4ab8706d275f90',
         endpoint: ['lists', 'items']
     }
-    var api = "https://gateway.marvel.com/v1/public/comics?apikey=";
+    var api = "https://gateway.marvel.com/v1/public/characters?apikey=";
     var auth = "9fc3988f672586da032a847df46e7861";
     var connect = api + auth;
     fetch(connect, data.option)
