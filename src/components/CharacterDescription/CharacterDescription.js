624068e7fa7cc99e7306d75caf9b29992dd52b38
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2021-06-28 06:52:12 -0400

    SCSS Edits

--- src/components/CharacterDescription/CharacterDescription.js
+++ src/components/CharacterDescription/CharacterDescription.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CharacterDescription.scss';
 const CharacterDescription = props => {
     return (
-        <section className="characters">
+        <section className="individual">
             <h2>{props.character.name}</h2>
             <img alt={props.character.name} src={props.character.thumbnail} height="300px" width="300px" />
             { props.method}
